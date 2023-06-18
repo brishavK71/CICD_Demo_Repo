56aab87d052c04cc9986a9b5ea79888fc4c6e3f5
brishavK71 <119672530+brishavK71@users.noreply.github.com>
2023-06-18 14:42:29 +0545

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
     return (
         <div className="jumbotron text-center">
             <h1>Company</h1>
-            <p>Welcome to blablabla</p>
+            <p>This is Testing phase</p>
             <form className="form-inline">
                 <div className="input-group">
                     <input type="email" className="form-control" size="50" placeholder="Email Address" required />
